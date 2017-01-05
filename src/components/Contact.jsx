9b6ec5a797f79ed01f6f8f10a02f24f2020058d2
Jonathan Weiß <bitbucket@jonathanweiss.net>
2017-01-05 09:44:34 +0100

    Add two more pages

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Contact = () => (
+  <div>
+    <h2>Contact us</h2>
+    <textarea />
+  </div>
+);
+
+export default Contact;
---
+++ src/components/Error404.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Error404 = () => (
+  <div>404 - page not found</div>
+);
+
+export default Error404;
