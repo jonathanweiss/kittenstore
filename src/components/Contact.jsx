3b1f96ca7de82bb1658be542850fac00467819f8
Jonathan Weiß <bitbucket@jonathanweiss.net>
2017-01-05 11:05:31 +0100

    Add content to components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const Contact = () => (
   <div>
     <h2>Contact us</h2>
-    <textarea />
+    <form>
+      <div className="form-group">
+        <label className="form-label" htmlFor="input-example-1">Name</label>
+        <input className="form-input" type="text" id="input-example-1" placeholder="Name" />
+      </div>
+      <div className="form-group">
+        <label className="form-label" htmlFor="input-example-3">Message</label>
+        <textarea className="form-input" id="input-example-3" placeholder="Textarea" rows="3" />
+      </div>
+      <div className="form-group">
+        <button className="btn btn-primary" type="submit">Submit</button>
+        <button className="btn btn-link" type="reset">Cancel</button>
+      </div>
+    </form>
   </div>
 );
 
--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Error404 = () => (
-  <div>404 - page not found</div>
+  <div>
+    <h1>404 - page not found</h1>
+    <img alt="Sad kitten" src="http://i94.photobucket.com/albums/l92/Ruas_2006/Caturday/Sad_Kitten_Is_Sad.jpg" />
+  </div>
 );
 
 export default Error404;
