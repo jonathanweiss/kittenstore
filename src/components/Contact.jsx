c5b38b0e3aaca866cab00ca668ab8a2f14ebfe89
Jonathan Weiß <bitbucket@jonathanweiss.net>
2017-01-06 17:45:53 +0100

    Make Contact a component with state

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
 
-const Contact = () => (
-  <div className="columns">
-    <div className="column col-12">
-      <h2>Contact us</h2>
-      <form>
-        <div className="form-group">
-          <label className="form-label" htmlFor="input-example-1">Name</label>
-          <input className="form-input" type="text" id="input-example-1" placeholder="Name" />
+class Contact extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modifiedContent: false,
+    };
+  }
+
+  markAsChanged(e) {
+    if (e.target.value.length > 0) {
+      this.setState({
+        modifiedContent: true,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="columns">
+        <div className="column col-12">
+          <h2>Contact us</h2>
+          <form>
+            <div className="form-group">
+              <label className="form-label" htmlFor="input-example-1">Name</label>
+              <input className="form-input" onChange={e => this.markAsChanged(e)} type="text" id="input-example-1" placeholder="Name" />
+            </div>
+            <div className="form-group">
+              <label className="form-label" htmlFor="input-example-3">Message</label>
+              <textarea className="form-input" onChange={e => this.markAsChanged(e)} id="input-example-3" placeholder="Textarea" rows="3" />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-primary" type="submit">Submit</button>
+              <button className="btn btn-link" type="reset">Cancel</button>
+            </div>
+          </form>
         </div>
-        <div className="form-group">
-          <label className="form-label" htmlFor="input-example-3">Message</label>
-          <textarea className="form-input" id="input-example-3" placeholder="Textarea" rows="3" />
-        </div>
-        <div className="form-group">
-          <button className="btn btn-primary" type="submit">Submit</button>
-          <button className="btn btn-link" type="reset">Cancel</button>
-        </div>
-      </form>
-    </div>
-  </div>
-);
+      </div>
+    );
+  }
+}
 
 export default Contact;
