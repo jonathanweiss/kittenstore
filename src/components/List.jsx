ab93da7e4304c974bd0e1e0c13d856eee08cffef
Jonathan Weiß <bitbucket@jonathanweiss.net>
2017-01-09 09:29:08 +0100

    Use code from other branch

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const headers = ['Name', 'Age', 'Weight', 'Price'];
+const headers = ['Name', 'Age', 'Weight', 'Gender', 'Price'];
 
 const renderHeader = (text, sortedBy, direction) => (
   <th key={text}>
@@ -9,12 +9,55 @@ const renderHeader = (text, sortedBy, direction) => (
   </th>
 );
 
-const List = (props) => {
-  const { slug, sortedBy, sortDirection } = props;
-  let cats = props.data.filter(cat => cat.breedSlug === slug);
+const renderProducts = (products, sortedBy, sortDirection) => (
+  <div className="columns">
+    <div className="column col-12">
+      <h2>All &ldquo;{products[0].breed}&rdquo;</h2>
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            {headers.map(text => renderHeader(text, sortedBy, sortDirection === 'desc' ? 'down' : 'up'))}
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => {
+            const { age, weight, gender, price } = product;
 
-  if (props.sortedBy) {
-    cats = cats.sort((a, b) => {
+            return (
+              <tr key={product.slug}>
+                <td>{product.name}</td>
+                <td>{age} weeks</td>
+                <td>{weight} oz</td>
+                <td>{gender === 'male' ? '♂' : ' ♀'}</td>
+                <td>${price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  </div>
+);
+
+const renderEmpty = slug => (
+  <div className="columns">
+    <div className="column col-12">
+      <section className="empty">
+        <i className="icon icon-baffled" />
+        <p className="empty-title">No product found for category &ldquo;{slug}&rdquo;.</p>
+        <p className="empty-meta">Try a different category.</p>
+        <button className="empty-action btn btn-primary">Browse for products</button>
+      </section>
+    </div>
+  </div>
+);
+
+const List = (props) => {
+  const { slug, type, sortedBy, sortDirection } = props;
+  let products = props.data.filter(product => product.breedSlug === slug);
+
+  if (sortedBy) {
+    products = products.sort((a, b) => {
       const trueValue = sortDirection === 'desc' ? -1 : 1;
       const falseValue = sortDirection === 'desc' ? 1 : -1;
 
@@ -22,37 +65,19 @@ const List = (props) => {
     });
   }
 
-  return (
-    <div className="columns">
-      <div className="column col-12">
-        <h2>All &ldquo;{cats[0].breed}&rdquo; cats</h2>
-        <table className="table table-striped table-hover">
-          <thead>
-            <tr>
-              {headers.map(text => renderHeader(text, sortedBy, sortDirection === 'desc' ? 'down' : 'up'))}
-            </tr>
-          </thead>
-          <tbody>
-            {cats.map(cat => (
-              <tr key={cat.slug}>
-                <td>{cat.name}</td>
-                <td>{cat.age} weeks</td>
-                <td>{cat.weight} oz</td>
-                <td>${cat.price}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+  return products.length ? renderProducts(products, sortedBy, sortDirection) : renderEmpty(slug, type);
 };
 
 List.propTypes = {
   slug: React.PropTypes.string.isRequired,
+  type: React.PropTypes.string.isRequired,
   data: React.PropTypes.array.isRequired,
   sortedBy: React.PropTypes.string,
   sortDirection: React.PropTypes.string,
+};
+
+List.contextTypes = {
+  location: React.PropTypes.object,
 };
 
 export default List;
