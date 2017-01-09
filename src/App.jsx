e756e4f5e947a8eb03a80afbf7eca7a78d9e45d1
Jonathan Weiß <bitbucket@jonathanweiss.net>
2017-01-09 07:12:24 +0100

    Show all components if flag is set

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
-
 import Homepage from './components/Homepage';
 
-const App = props => (
-  <Homepage amountOfProducts={3} data={props.data} />
-);
+import About from './components/About';
+import Cart from './components/Cart';
+import Category from './components/Category';
+import Contact from './components/Contact';
+import Detail from './components/Detail';
+import Error404 from './components/Error404';
+import List from './components/List';
+import Navigation from './components/Navigation';
+import SearchResult from './components/SearchResult';
+
+const SHOW_ALL_COMPONENTS = true;
+
+const App = (props) => {
+  if (SHOW_ALL_COMPONENTS) {
+    const { categories, products } = props.data;
+    const { cats } = products;
+
+    return (
+      <div>
+        <About />
+        <hr />
+        <Error404 />
+        <hr />
+        <Cart />
+        <hr />
+        <Contact />
+        <hr />
+        <Navigation />
+        <hr />
+        <SearchResult term="el" data={cats} />
+        <hr />
+        <Category desc="cats" items={categories.cats.items} />
+        <hr />
+        <List type="cats" slug="javanese" data={cats} />
+        <hr />
+        <Detail slug="twinkle" data={cats} />
+        <hr />
+      </div>
+    );
+  } else {
+    return (<Homepage amountOfProducts={3} data={props.data} />);
+  }
+};
 
 App.propTypes = {
   data: React.PropTypes.object,
