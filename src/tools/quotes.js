a20fb9a82a8789bb1662ecfccc4df22cd3bbb61f
Jonathan Weiß <bitbucket@jonathanweiss.net>
2017-01-20 17:03:16 +0100

    Fix index for getRandomQuote()

--- src/tools/quotes.js
+++ src/tools/quotes.js
@@ -196,7 +196,7 @@ const quotes = [
 ];
 
 export const getRandomQuote = () => {
-  const index = Math.round(Math.random() * quotes.length);
+  const index = Math.round(Math.random() * (quotes.length - 1));
   return quotes[index];
 };
 
