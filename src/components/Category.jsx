17170fe6fba32cecd2c3b11d60ca905e99ca7a10
Jonathan Weiß <bitbucket@jonathanweiss.net>
2017-01-24 21:51:06 +0100

    Prepare component for react-router

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
-const renderProduct = (name, slug, provideImageSource) => (
+// We'll remove the hashes when we integrate react-router ;)
+/* eslint-disable jsx-a11y/href-no-hash */
+const renderProduct = (name, slug, type, provideImageSource) => (
   <div key={slug} className="product--item">
-    <figure>
-      <img width="200" height="200" src={provideImageSource()} alt={name} />
-      <figcaption>{name}</figcaption>
-    </figure>
+    <a href="#" key={`/${type}/${slug}`}>
+      <figure>
+        <img width="200" height="200" src={provideImageSource()} alt={name} />
+        <figcaption>{name}</figcaption>
+      </figure>
+    </a>
   </div>
 );
+/* eslint-enable jsx-a11y/href-no-hash */
 
 const Category = (props) => {
-  const provideImageSource = props.desc === 'cats' ?
+  const type = props.desc.toLowerCase().split(' ').join('');
+  const provideImageSource = type === 'cats' ?
     index => `https://placekitten.com/200/200?image=${index}` :
     () => 'https://dummyimage.com/200x200/999/000.png';
 
@@ -18,7 +24,7 @@ const Category = (props) => {
     <div className="columns">
       <h2>{props.desc}</h2>
       <div className="product--container">
-        { props.items.map((entry, index) => renderProduct(entry.name, entry.slug, provideImageSource.bind(this, index))) }
+        { props.items.map((entry, index) => renderProduct(entry.name, entry.slug, type, provideImageSource.bind(this, index))) }
       </div>
     </div>
   );
