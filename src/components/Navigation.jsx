3e545a003974f40f9407431525b8d103e72551a5
Jonathan Weiß <bitbucket@jonathanweiss.net>
2017-01-24 07:45:52 +0100

    Add startSearch()

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -17,19 +17,34 @@ const renderNavItem = (entry, activePath) => {
   );
 };
 
-const Navigation = props => (
-  <header className="navbar">
-    <section className="navbar-section">
-      { props.items.map(entry => renderNavItem(entry, props.activePath)) }
-    </section>
-    <section className="navbar-section">
-      <div className="input-group input-inline">
-        <input className="form-input" type="text" placeholder="search" />
-        <button className="btn btn-primary input-group-btn">Search</button>
-      </div>
-    </section>
-  </header>
-);
+const startSearch = (textboxRef) => {
+  const term = textboxRef.value;
+
+  if (term.length > 0) {
+    // perform search
+  }
+};
+
+const Navigation = (props, context) => {
+  const { activePath } = props;
+  let textboxRef = null;
+
+  /* eslint-disable no-return-assign */
+  return (
+    <header className="navbar">
+      <section className="navbar-section">
+        { props.items.map(entry => renderNavItem(entry, activePath)) }
+      </section>
+      <section className="navbar-section">
+        <div className="input-group input-inline">
+          <input className="form-input" type="text" placeholder="search" ref={ref => textboxRef = ref} />
+          <button className="btn btn-primary input-group-btn" onClick={() => startSearch(textboxRef)}>Search</button>
+        </div>
+      </section>
+    </header>
+  );
+  /* eslint-enable no-return-assign */
+};
 
 Navigation.propTypes = {
   items: React.PropTypes.array,
