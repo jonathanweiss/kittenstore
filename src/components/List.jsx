6decaf4165ccd2e982273bd18228784288af5e48
Jonathan Weiß <bitbucket@jonathanweiss.net>
2017-01-25 12:38:54 +0100

    Correct prop type

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -89,7 +89,7 @@ List.propTypes = {
   slug: React.PropTypes.string.isRequired,
   type: React.PropTypes.string.isRequired,
   data: React.PropTypes.array.isRequired,
-  location: React.PropTypes.object,
+  location: React.PropTypes.string,
   sortedBy: React.PropTypes.string,
   sortDirection: React.PropTypes.string,
 };
