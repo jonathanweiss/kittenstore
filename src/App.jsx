a61bfc4c61311a71d670b2038295b924e2214034
Jonathan Weiß <bitbucket@jonathanweiss.net>
2017-01-25 12:58:55 +0100

    Add simple routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 
 import React from 'react';
+import { Match, Miss } from 'react-router';
 
 import Homepage from './components/Homepage';
 import About from './components/About';
@@ -13,50 +14,19 @@ import List from './components/List';
 import Navigation from './components/Navigation';
 import SearchResult from './components/SearchResult';
 
-// Toggle this flag to see all components
-const SHOW_ALL_COMPONENTS = false;
-
 const App = (props) => {
   const { navigationData, categories, products } = props.data;
   const { cats } = products;
 
-  if (SHOW_ALL_COMPONENTS) {
-    return (
-      <div>
-        <About />
-        <hr />
-        <Error404 />
-        <hr />
-        <Cart />
-        <hr />
-        <Contact />
-        <hr />
-        <Navigation activePath="/" items={navigationData} />
-        <hr />
-        <SearchResult term="el" data={cats} />
-        <hr />
-        <SearchResult term="yyyyyyyyyyy" data={cats} />
-        <hr />
-        <Category desc="cats" items={categories.cats.items} />
-        <hr />
-        <List type="cats" slug="javanese" data={cats} sortedBy="name" sortDirection="asc" />
-        <hr />
-        <List type="cats" slug="zzzzzzzzz" data={cats} sortedBy="name" sortDirection="asc" />
-        <hr />
-        <Detail slug="twinkle" data={cats} />
-        <hr />
-        <Detail slug="snickers" data={cats} showDetails={false} />
-        <hr />
-      </div>
-    );
-  } else { // eslint-disable-line
-    return (
-      <div>
-        <Navigation items={navigationData} activePath="/" />
-        <Homepage amountOfProducts={2} products={cats} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Match exactly pattern="/about" component={About} />
+      <Match exactly pattern="/contact" component={Contact} />
+      <Match exactly pattern="/cart" component={Cart} />
+
+      <Miss component={Error404} />
+    </div>
+  );
 };
 
 App.propTypes = {
